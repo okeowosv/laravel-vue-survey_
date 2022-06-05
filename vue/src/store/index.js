19a1a9f7b367fdbd1037559a09c54708a1c8dc8a
okeowosv <okeowoonline@gmail.com>
2022-06-05 23:42:23 +0100

    Setting up tailwind

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,1 +1,13 @@
-const store = createStore
+import {createStore} from "vuex";
+
+const store = createStore({
+    state:{
+        user:{data: {name:'Zura'}, token: null}
+    },
+    getters:{},
+    actions:{},
+    mutations:{},
+    modules:{}
+})
+
+export default store;
