d0fa81d3794057cc51ca8b838a0b0d683776ac31
okeowosv <okeowoonline@gmail.com>
2022-06-20 00:22:11 +0100

    Completed AuthController, Setting up api route

---
+++ vue/src/views/Dashboard.vue
@@ -0,0 +1,10 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+
+<template>
+<PageComponent title="Dashboard">Content Goes Here</PageComponent>
+</template>
+
+<script setup>
+import PageComponent from '../components/PageComponent.vue';
+</script>
+<style scoped></style>
